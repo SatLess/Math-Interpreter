6f9302111fc637009fced3aa5d5ba1a262051d0a
Sat <124409014+SatLess@users.noreply.github.com>
2025-05-22 22:04:04 -0300

    Finished with lexer, work now on parser

--- lexer.py
+++ lexer.py
@@ -1,5 +1,9 @@
 from calendar import c
 from enum import Enum
+from pickle import NONE
+from token import MINUS, PLUS
+from tokenize import String
+from unicodedata import digit
 
 
 WHITESPACE = ' \n\t'
@@ -33,9 +37,10 @@ class Token:
 #Actualy transforms shit into tokens
 class Lexer:
     source_code = ""
+    digit_lexeme = "" #For now that'll append digit for us 
     source_size = 0
     cursor_pos = 0
-    token_list = []
+    token_list: list = []
 
     def __init__(self, p_code):
         self.source_code = p_code
@@ -45,32 +50,44 @@ class Lexer:
         
     
     def advance(self):
-        if self.cursor_pos >= self.source_size: return ""
+        if self.cursor_pos >= self.source_size: return " "
         c = self.source_code[self.cursor_pos] 
         self.cursor_pos += 1
         return c
 
-    def get_previous_lexemme(self):
-        return self.source_code[self.cursor_pos - 1] 
+    def get_current_char(self):
+        if self.cursor_pos >= self.source_size: return " "
+        return self.source_code[self.cursor_pos] 
 
     def __generate_tokens(self):
         while self.cursor_pos < self.source_size:
             c = self.advance()
+            type = self.__match_token_type(c)
+            token: Token
 
             if c in WHITESPACE: continue
-            elif c in DIGITS:
-                token = Token(Token_Type.DIGITS, self.__generate_numbers())
-                self.token_list.append(token)
-            elif c in OPERATORS or c in SPECIAL_KEYS:
-                token = Token(self.__match_token_type(c), c)
-                self.token_list.append(token)
-            else:
-                print("Token " + c + " is invalid")
-                return
 
+            if type == Token_Type.DIGITS:
+                digits = c
+                next_digit = self.get_current_char()
+                while self.__match_token_type(next_digit) == Token_Type.DIGITS:
+                    digits += next_digit
+                    c = self.advance()
+                    next_digit = self.get_current_char()
+                token = Token(type,digits)
+            
+            elif type == None:
+                raise Exception("Why girls?")
+
+            else: token = Token(type,c)
+
+            self.token_list.append(token)
                 
 
     def __match_token_type(self,c):
+
+        if c in DIGITS: return Token_Type.DIGITS
+
         match c:
             case '+': return Token_Type.PLUS
             case '-': return Token_Type.MINUS
@@ -82,20 +99,14 @@ class Lexer:
 
             case '[': return Token_Type.OPEN_B
             case ']': return Token_Type.CLOSE_B
-
-    def __generate_numbers(self): #TODO need to fix how this works loool like it joins toghether not good tokens toghether ie no token num
-        num_digit = self.get_previous_lexemme()
-        if (num_digit in DIGITS == False): return ""
-        c = self.advance()
-        if(c in WHITESPACE): return num_digit
-        return num_digit + self.__generate_numbers() #Little bit confusing to understand, since we don't use c directly
-
+            
+            case _: return None
 
     def print_tokens(self):
         for i in self.token_list:
             print(i.lexeme)
         
-test = Lexer("123 + 789")
+test = Lexer("123 + 789 - (2 * 3)")
 
         
     
---
+++ parser.py
@@ -0,0 +1,8 @@
+from lexer import Token
+
+class Parser():
+    
+    token_list: list = []
+    current: Token
+
+    def consume(self): pass
